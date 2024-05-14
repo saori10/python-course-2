0f31edcf369b80c8499048cd37d64525cc6ecdb2
saori10 <saori.rg09@gmail.com>
2024-05-14 18:58:55 -0500

    i did the firts class

---
+++ 1-zen.py
@@ -0,0 +1,25 @@
+import this
+
+# TRADUCCION: 
+
+"""
+Lo bello es mejor que lo feo.
+Lo explícito es mejor que lo implícito.
+Lo simple es mejor que lo complejo.
+Complejo es mejor que complicado.
+Plano es mejor que anidado.
+Escaso es mejor que denso.
+La legibilidad cuenta.
+Los casos especiales no son lo suficientemente especiales como para romper las reglas.
+Aunque la practicidad vence a la pureza.
+Los errores nunca deben pasar en silencio.
+A menos que se silencie explícitamente.
+Ante la ambigüedad, rechace la tentación de adivinar.
+Debería haber una (y preferiblemente sólo una) forma obvia de hacerlo.
+Aunque puede que al principio no resulte obvio a menos que seas holandés.
+Ahora es mejor que nunca.
+Aunque nunca es mejor que *ahora mismo* ahora.
+Si la implementación es difícil de explicar, es una mala idea.
+Si la implementación es fácil de explicar, puede ser una buena idea.
+Los espacios de nombres son una gran idea: ¡hagamos más de eso!
+"""
